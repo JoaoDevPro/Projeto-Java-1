24a9fb993e89981e74b63f5ccc85af50b295eeb3
JOAO-VICTOR-SOARES-LIMA <j.soares@greylogix.com.br>
2024-08-20 19:43:04 -0300

    comitando

---
+++ demo/src/test/java/senai/Jornada/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package senai.Jornada.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
